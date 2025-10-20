4c6193cd0415babf2d58c291eef3ff1a749589e1
Shoaib1345 <shoaibahmedprogramming@gmail.com>
2025-10-20 09:16:09 +0500

    20. Valid Parentheses

---
+++ 20_Valid_Parentheses.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+        mapping = {')': '(', '}': '{', ']': '['}
+        for char in s:
+            if char in mapping:
+                if stack and stack[-1] == mapping[char]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(char)
+        return not stack
+obj = Solution()
+print(obj.isValid("()"))
+print(obj.isValid("()[]{}"))
+print(obj.isValid("(]"))
+print(obj.isValid("([])"))
+print(obj.isValid("([)]"))
